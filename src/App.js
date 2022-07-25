4e87bb65b7d5748617af1ea85facf5e01201f3b4
AliMoHoaMed <alilomoh16@gmail.com>
2022-07-26 00:27:01 +0200

    ggg

--- src/App.js
+++ src/App.js
@@ -3,22 +3,136 @@ import './App.css';
 
 function App() {
   return (
+    <body>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="c">
+      <p style={{color:'white' ,fontSize:'40px'}}>Hello , My Name Is Ali Mohamed</p>
+       <p style={{color:'white' ,fontSize:'40px'}}> I'M a <a style={{color : '#1E90FF'}}>Front-end developer</a> </p>
+          <div className='c' >
+
+<div className='center v circle' >  
+  <img src='me 2.png'  style={{width : '290px',height : '350px'}}/>
+  </div>
+  
+  </div>
+           </header>
+
+           <br/>
+       <div className='c v2'>  
+        <p style={{color:'white' ,fontSize:'40px'}}>About</p> 
+
+        <p style={{color:'white' ,fontSize:'20px'}}> I am a computer engineer and a front-end developer seeking for a knowledge and learning new 
+skills to get the job done in no time and with brand new designs
+         </p>
+<p style={{color:'white' ,fontSize:'20px'}}>  
+i can manage with different frameworks and try new ways to reach a new solution 
+</p>
+
+        </div>    
+
+        <br/><br/><div className='l'> </div>  
     </div>
+    <p style={{color:'white' ,fontSize:'50px'}}> Skills  </p>
+    <div className='v3'> 
+    <p style={{color:'white' ,fontSize:'30px'}}> Front end  </p>
+ <a className='a1'>   Html   </a>  <a className='a1'>  CSS  </a> <a className='a1'>   javaScript   </a>     <a className='a1'>  Dealing with Api    </a> 
+ <p style={{color:'white' ,fontSize:'30px'}}> React js   </p>
+ <a className='a1'>   Routing    </a>   <a className='a1'>   Hooks     </a>  <a className='a1'>   Dealing with Api     </a>   <a className='a1'>   Axios libary    </a>    <a className='a1'>   redux basics    </a>  
+ <p style={{color:'white' ,fontSize:'30px'}}> Java  </p>
+ <a className='a1'>   java basics    </a>   <a className='a1'>  ADF  java EE     </a>
+ <p style={{color:'white' ,fontSize:'30px'}}> Database </p>
+ <a className='a1'>   DB design     </a>   <a className='a1'>  SQL   </a>
+ <p style={{color:'white' ,fontSize:'30px'}}> Other skills </p>
+ <a className='a1'>  image processing knowledge with Open cv C++    </a>   
+ 
+ 
+    </div>
+<br/><div className='l'> </div>  
+<br/> 
+<p style={{color:'white' ,fontSize:'50px'}}> EXPERIENCE   </p>
+<div className='v3'> 
+<p style={{color:' rgb(168, 168, 168)' ,fontSize:'30px'}}> October 2021- June 2022  </p>
+<a className='a2'> Web development • health-care-app</a>
+<br/> 
+<a className='a3'> I worked as a front-end developer by using React.JS
+and web design using HTML and CSS and how to deal with API</a>
+
+<br/><br/> 
+
+<p style={{color:' rgb(168, 168, 168)' ,fontSize:'30px'}}> February 2021 - May 2021  </p>
+<a className='a2'>Database designer- “Database project” for (elections)</a>
+<br/> 
+<a className='a3'> I worked as a designer for the schema and build the database
+by using MySQL workbench</a>
+
+<br/><br/> 
+
+<p style={{color:' rgb(168, 168, 168)' ,fontSize:'30px'}}> July 2021 - same month  </p>
+<a className='a2'> “Cars rent web “. (‘course project’)
+</a>
+<br/> 
+<a className='a3'> Database and a front-end By using ADF, Java EE, html, CSS 
+    </a>
+
+</div>
+<br/><div className='l'> </div>  
+<br/>
+<div className='v3'> 
+
+<p style={{color:'white' ,fontSize:'50px'}}> LANGUAGE SKILLS  </p>
+<a className='a3'> Arabic (Native language)   </a>
+<br/><br/>
+<a className='a3'> English (very good) Reading Writing speaking conversation  </a>
+<br/><br/><a className='a3'> French (good)</a>
+
+
+</div>
+<br/><div className='l'> </div>  
+<br/>
+<p style={{color:'white' ,fontSize:'50px'}}> PROJECTS  </p>
+
+<div className='center2'> 
+
+<div className='grid-container'>  
+<div className='grid-item c'>
+<p style={{fontSize : '40px' , color:'white'}}> health care </p>
+  <img src='healthcare.png' style={{width:'600px' ,hight:'800px'}}/><a style={{color:'white'}} href='https://github.com/AliMoHoaMed/health-care-web'> go to project </a>
+  
+   </div>
+   <div className='grid-item'>
+   <p style={{fontSize : '40px' , color:'white'}}> movies app </p>
+<img src='movies.png' style={{width:'600px' ,hight:'800px'}} />  <a style={{color:'white'}} href='https://github.com/AliMoHoaMed/anime-moviess-app'> go to project </a>
+   </div>
+   <div className='grid-item'>
+   <p style={{fontSize : '40px' , color:'white'}}> Search app  </p>
+  <img src='stitch.png' style={{width:'600px' ,hight:'800px'}} />  <a style={{color:'white'}} href='https://github.com/AliMoHoaMed/search_resturant'> go to project </a>
+   </div>
+   <div className='grid-item'>
+   <p style={{fontSize : '40px' , color:'white'}}> image processing app </p>
+   
+<img src='alitool.png'  style={{width:'600px' ,hight:'800px'}}/><a style={{color:'white'}} href='https://github.com/AliMoHoaMed/image-processing-tools'> go to project </a>
+   </div>
+
+</div>
+
+</div>
+
+
+<footer>  
+<div className='m c'>  
+<p style={{color:'white' ,fontSize:'30px'}}> Contact  </p>
+<a className='a3'> phone :  (+20)1115808748-1551578003</a>
+
+<br/><br/><a className='a3'>Email : <a style={{color:'white'}} href='alilomoh16@gmail.com'>alilomoh16@gmail.com </a> </a>
+<br/><br/><a className='a3'>GitHub: <a style={{color:'white'}} href='https://github.com/AliMoHoaMed'>https://github.com/AliMoHoaMed </a> </a>
+<br/><br/><a className='a3'>LinkedIn: <a style={{color:'white'}} href='https://www.linkedin.com/in/ali-mohamed-3a95b5237/'>https://www.linkedin.com/in/ali-mohamed-3a95b5237/</a> </a>
+
+</div>
+
+
+</footer>
+
+    </body>
   );
 }
 
